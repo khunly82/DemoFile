182d999b3b2b5ea820580c15cb4eff15757f9b9d
Khun <khun.ly@bstorm.be>
2024-04-24 15:22:25 +0200

    Add project files.

---
+++ DemoFile/Program.cs
@@ -0,0 +1,84 @@
+﻿using DemoFile;
+using Newtonsoft.Json;
+
+const string filePath = @"C:\Users\K\source\repos\DemoFile\DemoFile\save.txt"; 
+
+List<Tache> taches = ChargerLesTaches();
+
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Menu");
+    Console.WriteLine("1. Ajouter une Tâche");
+    Console.WriteLine("2. Voir toutes les tâches");
+    Console.WriteLine("S. Sauver");
+    ConsoleKey key = Console.ReadKey(true).Key;
+
+    switch (key)
+    {
+        case ConsoleKey.NumPad1:
+            AjouterUneTache();
+            break;
+        case ConsoleKey.NumPad2:
+            VoirLesTaches();
+            break;
+        case ConsoleKey.S:
+            SauverLesTaches();
+            break;
+        default:
+            break;
+    }
+    Console.WriteLine("Press any key to continue ...");
+    Console.ReadKey();
+}
+
+List<Tache> ChargerLesTaches()
+{
+    List<Tache> result = new List<Tache>();
+    List<string> Lines = File.ReadAllLines(filePath).ToList();
+    foreach (string line in Lines)
+    {
+        result.Add(JsonConvert.DeserializeObject<Tache>(line));
+    }
+    return result;
+}
+
+void SauverLesTaches()
+{
+    List<string> result = new List<string>();
+    foreach (Tache tache in taches)
+    {
+        result.Add(JsonConvert.SerializeObject(tache));
+    }
+    File.WriteAllLines(filePath, result);
+}
+
+void VoirLesTaches()
+{
+    foreach (var tache in taches)
+    {
+        Console.WriteLine($"{tache.Name}, {tache.Description}, {tache.DeadLine}, {tache.Status}");
+        if(tache.UserName != null) 
+        {
+            Console.WriteLine($"Assignée à {tache.UserName}");
+        }
+        Console.WriteLine("__________________________________________________________________________");
+    }
+}
+
+void AjouterUneTache()
+{
+    Tache t = new Tache(
+        Question("Quel est le nom de la tache ?"),
+        Question("Quel est la description de la tache?"),
+        DateTime.Parse(Question("Quand doit-elle se terminer ?"))
+    );
+    taches.Add(t);
+}
+
+string Question(string message)
+{
+    Console.WriteLine($"{message}");
+    return Console.ReadLine();
+}
---
+++ DemoFile/Tache.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace DemoFile
+{
+    public class Tache
+    {
+        public Tache(string name, string description, DateTime deadLine)
+        {
+            Name = name;
+            Description = description;
+            DeadLine = deadLine;
+        }
+
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime DeadLine { get; set; }
+        public DateTime? StartDate { get; set; }
+        public string? UserName { get; set; } = null!;
+
+        public Status Status { get; set; } = Status.Created;
+    }
+
+    public enum Status
+    {
+        Created, InProgress, Finished
+    }
+}
